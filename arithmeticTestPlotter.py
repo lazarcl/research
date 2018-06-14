6b92847ff2ea97a0209b2a8919f9b534021ebbdf
lazarcl <lazarcl@whitman.edu>
2018-06-14 11:34:21 -0700

    update test plotter scripts for new run location

--- arithmeticTestPlotter.py
+++ arithmeticTestPlotter.py
@@ -1,6 +1,7 @@
 import pylab
 import pandas
 from matplotlib.backends.backend_pdf import PdfPages
+import glob
 
 '''
 helpful styling guide:
@@ -16,61 +17,87 @@ MAX_Y = 80
 
 #given filepath, return list of power data as FPs
 def getPowerFromFile(filePath):
-	colnames = ['power', 'temp', 'time', 'totalT', 'totalSamples']
-	data = pandas.read_csv(filePath, names=colnames, encoding='utf-8')
+  colnames = ['power', 'temp', 'time', 'totalT', 'totalSamples']
+  data = pandas.read_csv(filePath, names=colnames, encoding='utf-8')
 
-	power = data.power.tolist()[1:]
-	power = [float(power[i]) for i in range(len(power))]
-	return power
+  power = data.power.tolist()[1:]
+  power = [float(power[i]) for i in range(len(power))]
+  return power
 
-#name of test to make graph for
+#Load the data for given files and return graphed figure
 #  ex: FMAFP32, FMAFP64, AddInt32, MultFP32...
-def makeFigure(testStr):
-	power1 = getPowerFromFile("data/output" + testStr + "_1.csv")
-	power2 = getPowerFromFile("data/output" + testStr + "_2.csv")
+def makeFigure(file1, file2, testName):
+  power1 = getPowerFromFile(file1)
+  power2 = getPowerFromFile(file2)
 
-	f = pylab.figure()
-	ax = pylab.subplot(111)    
-	ax.spines["top"].set_visible(False)    
-	# ax.spines["bottom"].set_visible(False)    
-	ax.spines["right"].set_visible(False)    
-	# ax.spines["left"].set_visible(False)    
+  f = pylab.figure()
+  ax = pylab.subplot(111)    
+  ax.spines["top"].set_visible(False)    
+  # ax.spines["bottom"].set_visible(False)    
+  ax.spines["right"].set_visible(False)    
+  # ax.spines["left"].set_visible(False)    
 
-	pylab.plot(range(len(power1)), power1, "-b", label="Control Kernel", lw=LINE_WIDTH)
-	pylab.plot(range(len(power2)), power2, "-r", label="Test Kernel", lw=LINE_WIDTH)
+  pylab.plot(range(len(power1)), power1, "-b", label="Control Kernel", lw=LINE_WIDTH)
+  pylab.plot(range(len(power2)), power2, "-r", label="Test Kernel", lw=LINE_WIDTH)
 
 
-	pylab.xlabel('time(ms)')
-	pylab.ylabel('power(W)')
-	pylab.title(testStr + " Kernel Runs")
+  pylab.xlabel('time(ms)')
+  pylab.ylabel('power(W)')
+  pylab.title(testName + " Kernel Runs")
 
-	pylab.legend(loc="lower right")
-	pylab.ylim(0, MAX_Y)
+  pylab.legend(loc="lower right")
+  pylab.ylim(0, MAX_Y)
 
-	#save to seperate file
-	# f.savefig("data/"+ testStr + ".pdf", bbox_inches='tight')
+  #save to seperate file
+  # f.savefig("data/"+ testStr + ".pdf", bbox_inches='tight')
+  return f
 
-	return f
 
 #input: list of test strings to make graphs for
 #  ex: ["FMAFP32", "AddInt32"]
 #output: list of plots. One plot per input element
-def getListOfPlots(listOfTests):
-	out = []
-	for test in listOfTests:
-		out.append(makeFigure(test))
-	return out
+def getListOfPlots(folderPath, listOfTests):
+  outFigs = []
+
+  for test in listOfTests:
+    try:
+      file1 = folderPath + "output" + test + "_1.csv"
+      file2 = folderPath + "output" + test + "_2.csv"
+      fig = makeFigure(file1, file2, test)
+      outFigs.append(fig)
+    except FileNotFoundError as err:
+      print("Error Plotting Arithmetic Tests: \n  " + str(err).replace("File b", '')+". Continuing..." )
+  
+  return outFigs
 
 #given a file name to save to, and a list of figures, save to one pdf
 def saveFigureList(figs, filePath):
-	pp = PdfPages(filePath)
-	for fig in figs:
-		pp.savefig(fig, dpi=SAVE_DPI)
-	pp.close()
+  pp = PdfPages(filePath)
+  for fig in figs:
+    pp.savefig(fig, dpi=SAVE_DPI)
+  pp.close()
 
-testNames = ["AddFP32", "AddFP64", "AddInt32", "FMAFP32", "FMAFP64", "MultFP32", "MultFP64", "MultInt32"]
-figs = getListOfPlots(testNames)
-saveFigureList(figs, "data/arithmeticGraphs.pdf")
+
+if __name__ == "__main__":
+  testNames = ["AddFP32", "AddFP64", "AddInt32", "FMAFP32", "FMAFP64", "MultFP32", "MultFP64", "MultInt32"]
+  folderPaths = glob.glob("testRuns/*/")
+
+  if len(folderPaths) == 0:
+    print("Can't plot arithmetic data. No folders in './testRuns/'")
+  else:
+    print("plotting arithmetic graphs for folders:")
+    for f in folderPaths:
+      print("  "+f)
+
+  for folderPath in folderPaths:
+    figs = getListOfPlots(folderPath, testNames)
+    if len(figs) == 0:
+      print("Arithmeic test graph not made. No figures plotted")
+      exit(1)
+    else:
+      print("saving "+len(figs)+" arithmetic test graphs")
+    savePath = folderPath + "arithmeticGraphs.pdf"
+    saveFigureList(figs, savePath)
 
 
 
--- basePowTestPlotter.py
+++ basePowTestPlotter.py
@@ -20,94 +20,98 @@ colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', "tab:grey"]
 
 #given filepath, return list of power data as FPs
 def getPowerFromFile(filePath):
-	colnames = ['power', 'temp', 'time', 'totalT', 'totalSamples']
-	data = pandas.read_csv(filePath, names=colnames, encoding='utf-8')
+  colnames = ['power', 'temp', 'time', 'totalT', 'totalSamples']
+  data = pandas.read_csv(filePath, names=colnames, encoding='utf-8')
 
-	power = data.power.tolist()[1:]
-	power = [float(power[i]) for i in range(len(power))]
-	return power
+  power = data.power.tolist()[1:]
+  power = [float(power[i]) for i in range(len(power))]
+  return power
 
 #name of test to make graph for
 #  ex: FMAFP32, FMAFP64, AddInt32, MultFP32...
 def makeFigure(testPathTups, supTitle, subTitle):
-	powerLists = []
-	for i in range(len(testPathTups)):
-		powerLists.append(getPowerFromFile(testPathTups[i][0]))
+  powerLists = []
+  for i in range(len(testPathTups)):
+    powerLists.append(getPowerFromFile(testPathTups[i][0]))
 
-	f = pylab.figure()
-	ax = pylab.subplot(111)    
-	ax.spines["top"].set_visible(False)    
-	ax.spines["right"].set_visible(False)    
-	
-	for i in range(len(powerLists)):
-		pylab.plot(range(len(powerLists[i])), powerLists[i], colors[i], label="workload of "+str(i+1)+"x", lw=LINE_WIDTH)
+  f = pylab.figure()
+  ax = pylab.subplot(111)    
+  ax.spines["top"].set_visible(False)    
+  ax.spines["right"].set_visible(False)    
+  
+  for i in range(len(powerLists)):
+    pylab.plot(range(len(powerLists[i])), powerLists[i], colors[i], label="workload of "+str(i+1)+"x", lw=LINE_WIDTH)
 
-	pylab.xlabel('time(ms)')
-	pylab.ylabel('power(W)')
-	pylab.suptitle(supTitle)
-	pylab.title(subTitle, fontsize=8)
+  pylab.xlabel('time(ms)')
+  pylab.ylabel('power(W)')
+  pylab.suptitle(supTitle)
+  pylab.title(subTitle, fontsize=8)
 
-	pylab.legend(loc="lower right")
-	pylab.ylim(0, MAX_Y)
+  pylab.legend(loc="lower right")
+  pylab.ylim(0, MAX_Y)
 
-	#save to seperate file
-	# f.savefig("data/"+ testStr + ".pdf", bbox_inches='tight')
+  #save to seperate file
+  # f.savefig("data/"+ testStr + ".pdf", bbox_inches='tight')
 
-	return f
+  return f
 
 #input: list of test strings to make graphs for
 #  ex: ["FMAFP32", "AddInt32"]
 #output: list of plots. One plot per input element
 def getListOfPlots(listOfTests):
-	out = []
-	for test in listOfTests:
-		out.append(makeFigure(test))
-	return out
+  plots = []
+  for test in listOfTests:
+    plots.append(makeFigure(test))
+  return plots
 
 #given a file name to save to, and a list of figures, save to one pdf
 def saveFigureList(figs, filePath):
-	pp = PdfPages(filePath)
-	for fig in figs:
-		pp.savefig(fig, dpi=SAVE_DPI)
-	pp.close()
+  pp = PdfPages(filePath)
+  for fig in figs:
+    pp.savefig(fig, dpi=SAVE_DPI)
+  pp.close()
 
 
 def createTestPathTuples(paths, folderPath):
-	pathTups = []
-	for i in range(len(paths)):
-		name = paths[i].replace(folderPath+"output", "").replace(".csv", "")
-		pathTups.append( (paths[i], name) )
-	return pathTups
+  pathTups = []
+  for i in range(len(paths)):
+    name = paths[i].replace(folderPath+"output", "").replace(".csv", "")
+    pathTups.append( (paths[i], name) )
+  return pathTups
 
 
 def makeBasePow2Graph(folderPath):
-	if folderPath[-1] != "/":
-		folderPath+="/"
+  if folderPath[-1] != "/":
+    folderPath+="/"
 
-	supTitle = "Base Power 2nd Approach Run"
-	subTitle = "Linearly changing number of blocks per run"
-	paths = glob.glob(folderPath+"*.csv")
+  supTitle = "Base Power 2nd Approach Run"
+  subTitle = "Linearly changing number of blocks per run"
+  paths = glob.glob(folderPath+"outputBlockScalar_*.csv")
 
-	pathTuples = createTestPathTuples(paths, folderPath)
-	fig = makeFigure(pathTuples, supTitle, subTitle)
-	fig.savefig(folderPath+"resultsGraphBP2.pdf", dpi=SAVE_DPI)
+  pathTuples = createTestPathTuples(paths, folderPath)
+  fig = makeFigure(pathTuples, supTitle, subTitle)
+  fig.savefig(folderPath+"resultsGraphBP2.pdf", dpi=SAVE_DPI)
 
 def makeBasePow1Graph(folderPath):
-	if folderPath[-1] != "/":
-		folderPath+="/"
+  if folderPath[-1] != "/":
+    folderPath+="/"
 
-	supTitle = "Base Power 1st Approach Run"
-	subTitle = "Changing number of concurrent blocks per SM"
-	paths = glob.glob(folderPath+"*.csv")
+  supTitle = "Base Power 1st Approach Run"
+  subTitle = "Changing number of concurrent blocks per SM"
+  paths = glob.glob(folderPath+"outputBlcksPerSM_*.csv")
 
-	pathTuples = createTestPathTuples(paths, folderPath)
-	fig = makeFigure(pathTuples, supTitle, subTitle)
-	fig.savefig(folderPath+"resultsGraphBP1.pdf", dpi=SAVE_DPI)
+  pathTuples = createTestPathTuples(paths, folderPath)
+  fig = makeFigure(pathTuples, supTitle, subTitle)
+  fig.savefig(folderPath+"resultsGraphBP1.pdf", dpi=SAVE_DPI)
 
 
 
 if __name__ == "__main__":
-	makeBasePow2Graph("data/basePow2_6/")
+  for folderPath in glob.glob("testRuns/run*/"):
+    makeBasePow2Graph(folderPath)
+  for folderPath in glob.glob("testRuns/run*/"):
+    makeBasePow2Graph(folderPath)
+
 
 
 

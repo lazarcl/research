290e4fc7c339996b8210943301f4d0521a3e9c91
lazarcl <lazarcl@whitman.edu>
2018-07-10 11:30:17 -0700

    create MemoryTestBase class

--- memoryTests.cu
+++ memoryTests.cu
@@ -15,18 +15,73 @@ void globalMemKernel(int n, int iterateNum, volatile T *x) {
   x[thread] = a;
 }
 
+
 template <typename T>
-class GlobalMemTest : public ArithmeticTestBase<T> {
+class MemoryTestBase {
+public: 
+
+  T *d_x;
+  int n;
+  int iterNum;
+  int numBlocks;
+  int blockSize;
+  int numBlockScale;
+  int opsPerIteration; //number of operations in one iteration. Not including loop calculations
+
+  MemoryTestBase(int blockSize, int iterNum)
+    : iterNum(iterNum), blockSize(blockSize), numBlockScale(360)
+  { opsPerIteration = 0;}
+  MemoryTestBase(int blockSize, int iterNum, int numBlockScale)
+    : iterNum(iterNum), blockSize(blockSize), numBlockScale(numBlockScale)
+  { opsPerIteration = 0;}
+
+  ~MemoryTestBase() {
+    CUDA_ERROR( cudaFree(d_x) );
+  }
+
+  void kernelSetup(cudaDeviceProp deviceProp) {
+    numBlocks = deviceProp.multiProcessorCount * numBlockScale;
+    n = numBlocks * blockSize;
+    CUDA_ERROR( cudaMalloc(&d_x, n*sizeof(T)) ); 
+    createData<T><<<numBlocks, blockSize>>>(n, d_x);
+  }
+
+  //get the number of threads launched in the kernel. Must be 
+  //called after kernelSetup() or the neccisary fields may not be initialized
+  int getNumThreads() {
+    return numBlocks * blockSize;
+  }
+
+  //return the number of operations that are executed in the kernel's loop
+  //for the specified number of operations.
+  //Ex: 6 operations per iteration * 1000000 iterations = 6000000 operations
+  int getOpsPerThread() {
+    return opsPerIteration * iterNum;
+  }
+
+  void runKernel();
+
+  void CUDA_ERROR(cudaError_t e) {
+    if (e != cudaSuccess) {
+      printf("cuda error in test class: \"%s\"\n", cudaGetErrorString(e));
+    }
+  } 
+
+};
+
+
+template <typename T>
+class GlobalMemTest : public MemoryTestBase<T> {
 public:
   GlobalMemTest(int blockSize, int iterNum) 
-      : ArithmeticTestBase<T>(blockSize, iterNum) 
+      : MemoryTestBase<T>(blockSize, iterNum) 
   {this->opsPerIteration = 6;}
   GlobalMemTest(int blockSize, int iterNum, int numBlockScale) 
-      : ArithmeticTestBase<T>(blockSize, iterNum, numBlockScale) 
+      : MemoryTestBase<T>(blockSize, iterNum, numBlockScale) 
   {this->opsPerIteration = 6;}
 
   void kernelSetup(cudaDeviceProp deviceProp) {
-    ArithmeticTestBase<T>::kernelSetup(deviceProp);
+    MemoryTestBase<T>::kernelSetup(deviceProp);
   }
 
   void runKernel() {
--- memoryTests.h
+++ memoryTests.h
@@ -7,9 +7,43 @@ template <typename T>
 __global__
 void globalMemKernel(int n, int iterateNum, volatile T *x);
 
+template <typename T>
+class MemoryTestBase {
+public: 
+
+  T *d_x;
+  int n;
+  int iterNum;
+  int numBlocks;
+  int blockSize;
+  int numBlockScale;
+  int opsPerIteration; //number of operations in one iteration. Not including loop calculations
+
+  MemoryTestBase(int blockSize, int iterNum);
+  MemoryTestBase(int blockSize, int iterNum, int numBlockScale);
+
+  ~MemoryTestBase();
+
+  void kernelSetup(cudaDeviceProp deviceProp);
+
+  //get the number of threads launched in the kernel. Must be 
+  //called after kernelSetup() or the neccisary fields may not be initialized
+  int getNumThreads();
+
+  //return the number of operations that are executed in the kernel's loop
+  //for the specified number of operations.
+  //Ex: 6 operations per iteration * 1000000 iterations = 6000000 operations
+  int getOpsPerThread();
+
+  void runKernel();
+
+  void CUDA_ERROR(cudaError_t e);
+
+};
+
 
 template <typename T>
-class GlobalMemTest : public ArithmeticTestBase<T> {
+class GlobalMemTest : public MemoryTestBase<T> {
 public:
   GlobalMemTest(int blockSize, int iterNum);
   GlobalMemTest(int blockSize, int iterNum, int numBlockScale);
